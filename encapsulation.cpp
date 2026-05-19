50a8ae2134312c7e613c4a2818cffb774369b7db
Frhanarka <frhanarka@gmail.com>
2026-05-19 11:15:04 +0700

    class remotelampu

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 class remotelampu {
 private:
+    string SaklarNo[3]; // Assuming there are 3 switches
+
+public:
+    void setSaklarNo(int no, string value) {
+        SaklarNo[no] = value;
+    }
+    string getSaklarNo(int no) {
+        return SaklarNo[no]; 
+    }
+};
