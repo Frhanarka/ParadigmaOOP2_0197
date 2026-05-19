5ecd443f289ff90e53db5e8b5f2d909ded33ff77
Frhanarka <frhanarka@gmail.com>
2026-05-19 11:35:15 +0700

    public rekening bank

--- praktikum.cpp
+++ praktikum.cpp
@@ -5,3 +5,11 @@ class RekeningBank {
 protected:
     string nama;
     int saldo;
+
+public:
+    RekeningBank(string nama, int saldo) {
+        this->nama = nama;
+        this->saldo = saldo;
+    }
+
+    
